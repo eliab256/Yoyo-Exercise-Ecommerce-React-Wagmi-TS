10d41140efcee33150e5bc0f009521da99d82f2c
b-elia <bordonielia96@gmail.com>
2025-06-26 17:30:27 +0200

    eliminate header prop and move all with redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 import Header from './Components/Header';
 import Gallery from './Components/Gallery';
 import YourProductsPage from './Components/YourProductsPage';
-import { useSelector, useDispatch } from 'react-redux';
-import { setCurrentPage, type PageState } from './redux/pagesSlice';
-import { useCallback } from 'react';
+import { useSelector } from 'react-redux';
+import { type PageState } from './redux/pagesSlice';
 
 function App() {
-    const dispatch = useDispatch();
     const currentOpenPage = useSelector(
         (state: { currentPage: { currentPage: PageState } }) => state.currentPage.currentPage
     );
-
-    const handleGalleryClick = useCallback(() => {
-        dispatch(setCurrentPage('gallery'));
-    }, [dispatch]);
-
-    const handleYourProductsClick = useCallback(() => {
-        dispatch(setCurrentPage('yourProducts'));
-    }, [dispatch]);
 
     const pageComponents = {
         gallery: <Gallery />,
@@ -26,7 +16,7 @@ function App() {
 
     return (
         <div>
-            <Header onGalleryClick={handleGalleryClick} onYourProductClick={handleYourProductsClick} />
+            <Header />
             <main className="relative w-full mt-12 sm:mt-14 md:mt-14 lg:mt-16 xl:mt-18 bg-[url(/WebSite-Background.jpg)] bg-cover bg-center bg-no-repeat min-h-screen bg-fixed">
                 {pageComponents[currentOpenPage]}
             </main>
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,11 +1,9 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit';
+import { useDispatch } from 'react-redux';
+import { setCurrentPage } from '../redux/pagesSlice';
 
-interface HeaderProps {
-    onGalleryClick: () => void;
-    onYourProductClick: () => void;
-}
-
-const Header: React.FC<HeaderProps> = ({ onGalleryClick, onYourProductClick }) => {
+const Header: React.FC = () => {
+    const dispatch = useDispatch();
     return (
         <header className="flex items-center justify-between p-4 fixed top-0 left-0 w-full bg-white shadow-md z-50  h-12 sm:h-14 md:h-14 lg:h-16 xl:h-18">
             <div className="flex items-center space-x-2 cursor-default">
@@ -15,7 +13,7 @@ const Header: React.FC<HeaderProps> = ({ onGalleryClick, onYourProductClick }) =
             <div>
                 <button
                     className="bg-green-500 text-black px-4 py-2 rounded-xl hover:bg-green-300 cursor-pointer"
-                    onClick={onGalleryClick}
+                    onClick={() => dispatch(setCurrentPage('gallery'))}
                 >
                     Gallery
                 </button>
@@ -23,7 +21,7 @@ const Header: React.FC<HeaderProps> = ({ onGalleryClick, onYourProductClick }) =
             <div>
                 <button
                     className="bg-green-500 text-black px-4 py-2 rounded-xl hover:bg-green-300 cursor-pointer"
-                    onClick={onYourProductClick}
+                    onClick={() => dispatch(setCurrentPage('yourProducts'))}
                 >
                     Your Products
                 </button>
