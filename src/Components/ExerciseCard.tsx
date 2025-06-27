e69d8f975b6fb456d926d7ec51c5705ae338693f
b-elia <bordonielia96@gmail.com>
2025-06-27 15:10:38 +0200

    fixing resp design on header and exercise card

--- src/Components/ExerciseCard.tsx
+++ src/Components/ExerciseCard.tsx
@@ -36,34 +36,42 @@ const ExerciseCard: React.FC<ExerciseCardProps> = ({ exerciseProp }) => {
             onClick={() => dispatch(setSelectedExercise(id))}
             role="button"
             className={`p-4 rounded-xl bg-violet-100 relative flex flex-col items-center text-center shadow-lg transition-all 
-                hover:bg-violet-200 active:bg-violet-300 shadow-green-200 cursor-pointer
-               `}
+      hover:bg-violet-200 active:bg-violet-300 shadow-green-200 cursor-pointer`}
         >
-            <h2 className="text-xl font-semibold mb-2">{name}</h2>
-            <img src={imageUrl} alt={name} className="w-full max-w-[200px] h-auto object-cover rounded-md mb-3" />
+            <h2 className="text-base sm:text-lg md:text-xl font-semibold mb-2">{name}</h2>
+
+            <img
+                src={imageUrl}
+                alt={name}
+                className="w-full max-w-[140px] sm:max-w-[160px] md:max-w-[180px] lg:max-w-[200px] h-auto object-cover rounded-md mb-3"
+            />
+
             {!alreadyPurchased && (
                 <>
-                    <p className="text-gray-600 mb-2">{description}</p>
-                    <p className="font-bold text-violet-600 mb-6">{price} ETH</p>
-                    <p className="font-bold text-zinc-600 mb-6">≈ {usdPrice} $</p>
+                    <p className="text-xs sm:text-sm md:text-base text-gray-600 mb-2">{description}</p>
+                    <p className="font-bold text-violet-600 mb-3 text-sm sm:text-base md:text-lg">{price} ETH</p>
+                    <p className="font-bold text-zinc-600 mb-6 text-sm sm:text-base md:text-lg">≈ {usdPrice} $</p>
                 </>
             )}
+
             {alreadyPurchased && currentOpenPage === 'gallery' && (
                 <>
-                    <p className="text-gray-600 mb-2">{description}</p>
-                    <p className="font-bold text-violet-600 mb-6">{price} ETH</p>
-                    <p className="font-bold text-zinc-600 mb-6">≈ {usdPrice} $</p>
-                    <div className="absolute bottom-2 right-2 color-violet-600">
-                        <ShoppingCartIcon className="w-10 h-10"></ShoppingCartIcon>
+                    <p className="text-xs sm:text-sm md:text-base text-gray-600 mb-2">{description}</p>
+                    <p className="font-bold text-violet-600 mb-3 text-sm sm:text-base md:text-lg">{price} ETH</p>
+                    <p className="font-bold text-zinc-600 mb-6 text-sm sm:text-base md:text-lg">≈ {usdPrice} $</p>
+                    <div className="absolute bottom-2 right-2 text-violet-600">
+                        <ShoppingCartIcon className="w-6 h-6 sm:w-8 sm:h-8 md:w-10 md:h-10" />
                     </div>
                 </>
             )}
+
             {alreadyPurchased && currentOpenPage === 'yourProducts' && (
                 <>
-                    <p className="text-gray-600 mb-2">{deepDescription}</p>
+                    <p className="text-xs sm:text-sm md:text-base text-gray-600 mb-2">{deepDescription}</p>
                 </>
             )}
-            <span className="absolute bottom-2 left-2 text-xs text-gray-400">Id: {id}</span>
+
+            <span className="absolute bottom-2 left-2 text-[10px] sm:text-xs text-gray-400">Id: {id}</span>
         </div>
     );
 };
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -6,13 +6,16 @@ const Header: React.FC = () => {
     const dispatch = useDispatch();
     return (
         <header className="flex items-center justify-between p-4 fixed top-0 left-0 w-full bg-white shadow-md z-50  h-12 sm:h-14 md:h-14 lg:h-16 xl:h-18">
-            <div className="flex items-center space-x-2 cursor-default">
+            <div className="flex items-center space-x-2 cursor-default pr-2">
                 <img src="../public/Yoyo-Logo-Scritta-Scura.png" alt="Logo" className="h-10 w-10 rounded-full" />
-                <h1 className="text-xl font-bold">Yoyo Exercise Marketplace</h1>
+                <h1 className="hidden md:block text-center font-bold text-lg xl:text-2xl ">
+                    Yoyo Exercise Marketplace
+                </h1>
+                <h1 className="block md:hidden text-center font-bold text-sm sm:text-base">Yoyo</h1>
             </div>
             <div>
                 <button
-                    className="bg-green-500 text-black px-4 py-2 rounded-xl hover:bg-green-300 cursor-pointer"
+                    className="bg-green-500 text-black px-4 py-2 rounded-xl hover:bg-green-300 cursor-pointer text-sm sm:text-base md:text-lg lg:text-xl"
                     onClick={() => dispatch(setCurrentPage('gallery'))}
                 >
                     Gallery
@@ -20,13 +23,22 @@ const Header: React.FC = () => {
             </div>
             <div>
                 <button
-                    className="bg-green-500 text-black px-4 py-2 rounded-xl hover:bg-green-300 cursor-pointer"
+                    className="bg-green-500 text-black px-4 py-2 rounded-xl hover:bg-green-300 cursor-pointer text-sm sm:text-base md:text-lg lg:text-xl"
                     onClick={() => dispatch(setCurrentPage('yourProducts'))}
                 >
                     Your Products
                 </button>
             </div>
-            <ConnectButton />
+            <ConnectButton
+                accountStatus={{
+                    smallScreen: 'avatar',
+                    largeScreen: 'full',
+                }}
+                showBalance={{
+                    smallScreen: false,
+                    largeScreen: true,
+                }}
+            />
         </header>
     );
 };
